80ba02f70df02527c5653a8c3108c3ab70c28d7f
Dylan <78911744+Didiloy@users.noreply.github.com>
2024-11-20 11:56:27 +0100

    init

---
+++ index.ts
@@ -0,0 +1,130 @@
+import {
+  Provider,
+  ResolutionDetails,
+  EvaluationContext,
+  JsonValue,
+  OpenFeatureEventEmitter,
+  FlagValue,
+  StandardResolutionReasons,
+  ErrorCode,
+} from "@openfeature/server-sdk";
+import { FlagType, typeFactory } from "./type-factory.ts";
+
+export class SerliProvider implements Provider {
+  readonly metadata = {
+    name: SerliProvider.name,
+  } as const;
+
+  readonly runsOn = "server";
+  private readonly API_URL = "http://localhost:3328/api/v1/flags/";
+  private api_key = "";
+
+  constructor(api_key: string) {
+    this.api_key = api_key;
+  }
+  // emitter for provider events
+  events = new OpenFeatureEventEmitter();
+
+  resolveBooleanEvaluation(
+    flagKey: string,
+    defaultValue: boolean,
+    context: EvaluationContext,
+  ): Promise<ResolutionDetails<boolean>> {
+    return this.evaluate(flagKey, "boolean", defaultValue);
+  }
+
+  resolveStringEvaluation(
+    flagKey: string,
+    defaultValue: string,
+    context: EvaluationContext,
+  ): Promise<ResolutionDetails<string>> {
+    return this.evaluate<string>(flagKey, "string", defaultValue);
+  }
+
+  resolveNumberEvaluation(
+    flagKey: string,
+    defaultValue: number,
+    context: EvaluationContext,
+  ): Promise<ResolutionDetails<number>> {
+    return this.evaluate<number>(flagKey, "number", defaultValue);
+  }
+
+  resolveObjectEvaluation<JsonValue extends FlagValue>(
+    flagKey: string,
+    defaultValue: JsonValue,
+    context: EvaluationContext,
+  ): Promise<ResolutionDetails<JsonValue>> {
+    return this.evaluate<JsonValue>(flagKey, "object", defaultValue);
+  }
+
+  private async evaluate<T extends FlagValue>(
+    flagKey: string,
+    type: FlagType,
+    defaultValue: T,
+  ) {
+    return fetch(this.API_URL + flagKey, {
+      method: "GET",
+      headers: {
+        Authorization: `${this.api_key}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch flags: ${response.statusText}`);
+        }
+        const data = await response.json();
+
+        if (data.error) {
+          throw new Error(`Error when fetching flags: ${response.statusText}`);
+        }
+        const value = typeFactory(data.value, type);
+        if (typeof value !== "undefined" && typeof value !== type) {
+          return {
+            value: defaultValue,
+            reason: StandardResolutionReasons.CACHED,
+            errorCode: ErrorCode.TYPE_MISMATCH,
+            errorMessage: `flag key ${flagKey} is not of type ${type}`,
+          } as ResolutionDetails<T>;
+        }
+        return {
+          value: (typeof value !== type ? defaultValue : value) as T,
+          reason: StandardResolutionReasons.CACHED,
+        } as ResolutionDetails<T>;
+      })
+      .catch((error) => {
+        return {
+          value: defaultValue,
+          reason: StandardResolutionReasons.ERROR,
+          errorCode: ErrorCode.PROVIDER_FATAL,
+          errorMessage: error.message,
+        } as ResolutionDetails<T>;
+      });
+  }
+}
+
+const provider = new SerliProvider("api_key_2");
+const context = {};
+console.log(await provider.resolveBooleanEvaluation("my-flag", false, context));
+console.log(
+  await provider.resolveStringEvaluation(
+    "new-1",
+    "default resolved value",
+    context,
+  ),
+);
+console.log(await provider.resolveNumberEvaluation("new-flag-2", 0, context));
+
+console.log(
+  await provider.resolveObjectEvaluation("json-flag", { version: 4 }, context),
+);
+
+console.log(
+  "non existant flag (return default value of 0): ",
+  await provider.resolveNumberEvaluation("non-existant-flag", 0, context),
+);
+
+console.log(
+  "type mismatch flag: ",
+  await provider.resolveNumberEvaluation("my-flag", 0, context),
+);
